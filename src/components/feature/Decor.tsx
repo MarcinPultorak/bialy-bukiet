217d164f962128fdc930be0f3923c2e7d0758cd2
marcin <marcinpultorak@gmail.com>
2023-11-03 00:03:52 +0100

    rwd

--- src/components/feature/Decor.tsx
+++ src/components/feature/Decor.tsx
@@ -3,61 +3,43 @@ import Image from "next/image";
 
 const Decor: FC = () => {
   return (
-    <section className="w-full h-full bg-beige py-20">
+    <section className="w-full h-full bg-beige py-10 md:py-20">
       <div className="max-w-screen-2xl mx-auto flex flex-col px-10">
-        <h2 className="text-5xl mx-auto uppercase">Dekoracje</h2>
-        <div className="grid grid-cols-2 mt-20">
-          <div className="flex flex-col w-full px-20">
-            <div className="w-[310px] h-[260px] relative">
-              <Image
-                src={"/images/oferta1.jpg"}
-                alt="decor1"
-                fill
-                className="object-cover z-30"
-              />
-            </div>
-            <div className="w-[310px] h-[260px] relative self-end -mt-24">
-              <Image
-                src={"/images/oferta1.jpg"}
-                alt="decor1"
-                fill
-                className="object-cover z-30"
-              />
-            </div>
-            <div className="w-[310px] h-[260px] relative -mt-24">
-              <Image
-                src={"/images/oferta1.jpg"}
-                alt="decor1"
-                fill
-                className="object-cover z-30"
-              />
-            </div>
-            <div className="w-[310px] h-[260px] relative self-end -mt-24">
-              <Image
-                src={"/images/oferta1.jpg"}
-                alt="decor1"
-                fill
-                className="object-cover z-30"
-              />
-            </div>
+        <h2 className="text-3xl md:text-4xl lg:text-5xl mx-auto uppercase">
+          Dekoracje
+        </h2>
+        <div className="grid grid-flow-col grid-rows-2 xl:grid-rows-none xl:grid-cols-2 mt-10 lg:mt-20">
+          <div
+            className="w-full h-full relative"
+            style={{
+              boxShadow: "0px 4px 20px 0px rgba(0, 0, 0, 0.25)",
+            }}
+          >
+            <Image
+              src={"/images/gallery/20230128_144246.jpg"}
+              alt="decor1"
+              fill
+              className="object-cover z-30"
+            />
           </div>
-          <div className="flex items-center justify-end">
+
+          <div className="flex items-center">
             <div
-              className="px-10 py-20 text-center bg-white max-w-2xl"
+              className="px-5 sm:px-10 py-10 sm:py-20 text-center bg-white "
               style={{
                 boxShadow: "0px 4px 20px 0px rgba(0, 0, 0, 0.25)",
               }}
             >
-              <p className="text-2xl font-bold">
+              <p className="sm:text-lg md:text-xl lg:text-2xl font-bold">
                 Nim wkroczyłam na ścieżkę „organizatora” :) , zaczynałam od
                 dekoracji sal i kościołów. I nadal się tym zajmuję.
               </p>
-              <p className="text-lg mt-4">
+              <p className="text-xs sm:text-sm md:text-base lg:text-lg mt-4">
                 Jeżeli interesuje Was dekoracja na miejsca zaślubin, sali lub
                 szukacie dekoratorki na inna okoliczność to zapraszam Cię do
                 kontaktu po indywidualną wycenę. Oferuję:
               </p>
-              <ul className="text-lg mt-4">
+              <ul className="text-xs sm:text-sm md:text-base lg:text-lg mt-4">
                 <li>-dekorację miejsca ceremonii</li>
                 <li>-dekoracja sali weselnej</li>
                 <li>-dekoracje balonowe</li>
@@ -65,7 +47,7 @@ const Decor: FC = () => {
                 <li>-wypożyczenie dekoracji , pokrowców</li>
               </ul>
 
-              <p className="text-2xl font-bold mt-4">
+              <p className="sm:text-lg md:text-xl lg:text-2xl  font-bold mt-4">
                 Dekoracje sprawiają że ważny dla Was dzień, staje się magiczny i
                 niezwykły. Każdy dodatek, cieszący nasze oko, ma znaczenia, co
                 sprawia że Ty i Twoi goście czujecie się wyjątkowo.
--- src/components/feature/Quote.tsx
+++ src/components/feature/Quote.tsx
@@ -4,9 +4,15 @@ import { Parallax } from "react-parallax";
 
 const Quote: FC = () => {
   return (
-    <Parallax strength={600} bgImage="/images/cytat.jpg">
+    <Parallax
+      strength={600}
+      bgImage="/images/cytat.jpg"
+      bgImageStyle={{
+        objectFit: "cover",
+      }}
+    >
       <section
-        className="w-full h-screen relative"
+        className="w-full h-[600px] relative"
         style={{
           background: "rgba(0, 0, 0, 0.50)",
         }}
@@ -18,18 +24,18 @@ const Quote: FC = () => {
         className="object-cover -z-10"
       /> */}
 
-        <div className="max-w-screen-2xl mx-auto h-full w-full z-20 text-white text-4xl grid grid-cols-2">
-          <div className="flex flex-col justify-center">
+        <div className="max-w-screen-2xl mx-auto h-full w-full z-20 text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl grid grid-cols-1 lg:grid-cols-2">
+          <div className="flex flex-col justify-end lg:justify-center">
             <p className="italic tracking-widest px-10">
               “Żaden dzień się nie powtórzy, nie ma dwóch podobnych nocy, dwóch
               tych samych pocałunków, dwóch jednakich spojrzeń w oczy.”
             </p>
-            <p className="self-end text-2xl italic tracking-widest mt-4">
+            <p className="self-end sm:text-lg md:text-xl lg:text-2xl italic tracking-widest mt-4 pr-4 lg:pr-0">
               -Wisława Szymborska
             </p>
           </div>
           <div className="flex flex-col justify-center">
-            <p className="self-end tracking-widest">
+            <p className="self-center lg:self-end tracking-widest">
               Nie przegap swojego dnia...
             </p>
           </div>
