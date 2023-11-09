5a5b2dd891a80803ce666016852fa248bcbe2972
marcin <marcinpultorak@gmail.com>
2023-11-09 19:47:00 +0100

    dot

--- src/components/feature/Quote.tsx
+++ src/components/feature/Quote.tsx
@@ -31,6 +31,7 @@ const Quote: FC = () => {
         <div className="flex flex-col justify-center">
           <p className="self-center lg:self-end tracking-widest">
             Nie przegap swojego dnia...
+            <span>{isIOS ? "." : ""}</span>
           </p>
         </div>
       </div>
